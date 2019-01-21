3d068dcc1f0a8f82162e04e945bf973bb4524e8d
Damotorie <parksjin01@gmail.com>
2019-01-21 17:28:07 +0900

    Integrate function and provide prediction service
    This script can be run by Terminal
    Integrate all functions and provide prediction service according to user uploaded image

--- MNIST/model_load.py
+++ MNIST/model_load.py
@@ -3,18 +3,14 @@
 # Code is quoted from above site
 # 위의 사이트에서 코드 인용
 
-
-# Model Architecture
-# -----
-
-
-import psutil
-
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-
+import sys
+import numpy
+import one_hot_encoding
+import preprocessing
 import tensorflow as tf
 sess = tf.InteractiveSession()
+
+flatten = lambda x:numpy.array(x).reshape((1, 784))
 
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
@@ -32,65 +28,85 @@ def max_pool_2x2(x):
                         strides=[1, 2, 2, 1], padding='SAME')
 
 
+def predict_number(image_file, stored_model_name = "MNIST_MODEL_IMPROVED"):
+  x = tf.placeholder(tf.float32, shape=[None, 784])
+  y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
-x = tf.placeholder(tf.float32, shape=[None, 784])
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
+  # ---------------------------- Layer 1 ----------------------------
 
-# ---------------------------- Layer 1 ----------------------------
+  W_conv1 = weight_variable([5, 5, 1, 32])
+  b_conv1 = bias_variable([32])
 
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
+  x_image = tf.reshape(x, [-1,28,28,1])
 
-x_image = tf.reshape(x, [-1,28,28,1])
+  h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+  h_pool1 = max_pool_2x2(h_conv1)
 
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+  # ---------------------------- Layer 2 ----------------------------
 
-# ---------------------------- Layer 2 ----------------------------
+  W_conv2 = weight_variable([5, 5, 32, 64])
+  b_conv2 = bias_variable([64])
 
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+  h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+  h_pool2 = max_pool_2x2(h_conv2)
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+  # ---------------------------- Layer 3 ----------------------------
 
-# ---------------------------- Layer 3 ----------------------------
+  W_fc1 = weight_variable([7 * 7 * 64, 1024])
+  b_fc1 = bias_variable([1024])
 
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
+  h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+  h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+  # ---------------------------- Layer 4 ----------------------------
 
-# ---------------------------- Layer 4 ----------------------------
+  keep_prob = tf.placeholder(tf.float32)
+  h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+  W_fc2 = weight_variable([1024, 10])
+  b_fc2 = bias_variable([10])
 
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
+  y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+  cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
 
-y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+  # ---------------------------- Layer End ----------------------------
 
-# ---------------------------- Layer End ----------------------------
+  train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+  correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  sess.run(tf.global_variables_initializer())
 
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-sess.run(tf.global_variables_initializer())
+  saver = tf.train.Saver()
 
-saver = tf.train.Saver()
+  saver.restore(sess, stored_model_name)
 
-# for i in range(201):
-#   batch = mnist.train.next_batch(50)
-#   if i%100 == 0:
-#     train_accuracy = accuracy.eval(feed_dict={
-#         x:batch[0], y_: batch[1], keep_prob: 1.0})
-#     print("step %d, training accuracy %g, CPU Usage %s Memory Usage %s"%(i, train_accuracy, str(psutil.cpu_percent()), str(psutil.virtual_memory())))
-#   train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+  # print list(mnist.test.images[0])
+  # print mnist.test.labels[0]
+  #
+  # print("test accuracy %g"%accuracy.eval(feed_dict={
+  #     x: mnist.test.images[0], y_: mnist.test.labels[0], keep_prob: 1.0}))
 
-saver.restore(sess, "./MNIST_MODEL")
-print("test accuracy %g"%accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+  data = preprocessing.resizing(image_file, (28, 28))
+  data = flatten(one_hot_encoding.read(data))
+  # print data
+  # print list(data[0])
+
+  result = y_conv.eval(feed_dict = {x: data, keep_prob: 1}).tolist()
+  print result
+  return result[0].index(max(result[0]))
+
+
+if __name__ == '__main__':
+  if len(sys.argv) == 2:
+    result = predict_number(sys.argv[1])
+
+  elif len(sys.argv) == 3:
+    result = predict_number(sys.argv[1], sys.argv[2])
+
+  else:
+      result = "USAGE: ./model_load.py <image name> <Trained model --optional>"
+
+  print result
+
+  # print predict_number("../TC/3-6.jpg")
