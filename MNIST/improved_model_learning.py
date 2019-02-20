df71b47f734b6e173f73df41e7c26ec58f0c29ee
Damotorie <parksjin01@gmail.com>
2019-02-20 14:20:51 +0900

    Update improved_model_learning.py

--- MNIST/improved_model_learning.py
+++ MNIST/improved_model_learning.py
@@ -77,18 +77,18 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-# sess.run(tf.global_variables_initializer())
+sess.run(tf.global_variables_initializer())
 
 saver = tf.train.Saver()
-saver.restore(sess, file_path + "/MNIST_MODEL_IMPROVED")
+# saver.restore(sess, file_path + "/MNIST_MODEL_IMPROVED")
 
-# for i in range(10001):
-#   batch = mnist.train.next_batch(50)
-#   if i%100 == 0:
-#     train_accuracy = accuracy.eval(feed_dict={
-#         x:batch[0], y_: batch[1], keep_prob: 1.0})
-#     print("step %d, training accuracy %g, CPU Usage %s Memory Usage %s"%(i, train_accuracy, str(psutil.cpu_percent()), str(psutil.virtual_memory())))
-#   train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+for i in range(10001):
+  batch = mnist.train.next_batch(50)
+  if i%100 == 0:
+    train_accuracy = accuracy.eval(feed_dict={
+        x:batch[0], y_: batch[1], keep_prob: 1.0})
+    print("step %d, training accuracy %g, CPU Usage %s Memory Usage %s"%(i, train_accuracy, str(psutil.cpu_percent()), str(psutil.virtual_memory())))
+  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
 print("test accuracy %g"%accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
